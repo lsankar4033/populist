d70fde540e0e40ffa2c8d07cf47580ce607873a5
lsankar4033 <lsankar4033@gmail.com>
2017-11-18 18:56:57 -0800

    Remove erroneous comments

--- test/TestVoter.sol
+++ test/TestVoter.sol
@@ -6,18 +6,18 @@ import "../contracts/Voter.sol";
 
 contract TestVoter {
   function testInitialVotes() public {
-    Voter voter = new Voter(); //Voter(DeployedAddresses.Voter());
+    Voter voter = new Voter();
     Assert.equal(voter.getVotes("foo"), 0, "All initial votes should be 0");
   }
 
   function testVote() public {
-    Voter voter = new Voter(); //Voter(DeployedAddresses.Voter());
+    Voter voter = new Voter();
     voter.vote("foo");
     Assert.equal(voter.getVotes("foo"), 1, "Voting should increment vote count");
   }
 
   function testIndependentVoting() public {
-    Voter voter = new Voter(); // Voter(DeployedAddresses.Voter());
+    Voter voter = new Voter();
     voter.vote("foo");
     voter.vote("bar");
     Assert.equal(voter.getVotes("foo"), 1, "Voting should be independent among startups (foo)");

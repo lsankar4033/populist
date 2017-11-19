1d5ecdf70bb289c1ff8b99f3e6bb701ebd9d21f7
lsankar4033 <lsankar4033@gmail.com>
2017-11-18 18:44:09 -0800

    Add some initial tests and fix re-initialization logic on endVote

--- contracts/Voter.sol
+++ contracts/Voter.sol
@@ -5,8 +5,15 @@ import "./History.sol";
 contract Voter {
   History public history;
 
+  // Mapping of startup to votes in this cycle. This mapping is required to only contain state for this time
+  // period's vote (i.e. doesn't have legacy voting information). This requires that we clear it out at every
+  // 'endVote' call.
   mapping (string => uint) votes;
+
   string[] startups;
+  // Number of valid elements in the startup array. Allows us to re-initialize the array by changing the value
+  // of this pointer, instead of setting the array to an empty array (which requires a lot of gas).
+  uint numStartups = 0;
 
   function Voter() public {
     history = new History();
@@ -14,22 +21,31 @@ contract Voter {
 
   function vote(string startup) public {
     if (votes[startup] == 0) {
-      startups.push(startup);
+      addStartup(startup);
     }
 
     votes[startup] += 1;
+  }
+
+  function addStartup(string startup) private {
+    if (numStartups == startups.length) {
+      startups.length += 1;
+    }
+
+    startups[numStartups] = startup;
+    numStartups += 1;
   }
 
   function getVotes(string startup) view public returns(uint) {
     return votes[startup];
   }
 
-  // Figures out the startup that's won the current vote and commit it to history.
-  // NOTE: Maybe date computation could happen within contract
+  // Figures out the startup that's won the current vote and commit it to history. Also re-initializes
+  // relevant state for next time period's voting.
   function endVote(string dateStr) public {
     string memory bestStartup;
     uint mostVotes = 0;
-    for (uint i = 0; i < startups.length; i++) {
+    for (uint i = 0; i < numStartups; i++) {
       string memory startup = startups[i];
       if (votes[startup] > mostVotes) {
         bestStartup = startup;
@@ -37,8 +53,23 @@ contract Voter {
       }
     }
 
+    // NOTE: Maybe we want to store some sort of 'null' record when there was no winner?
     if (mostVotes > 0) {
       history.archiveRecord(dateStr, bestStartup, mostVotes);
     }
+
+    reinitializeState();
+  }
+
+  function reinitializeState() private {
+    // This is expensive, but it's better to have this mapping up-to-date for all 'getVotes' and 'vote' calls
+    // so that those are O(1) because they'll be called with much higher frequency.
+    for (uint i = 0; i < numStartups; i++) {
+      string memory startup = startups[i];
+      votes[startup] = 0;
+    }
+
+    // This is all we need to do to re-initialize the array.
+    numStartups = 0;
   }
 }
---
+++ test/TestVoter.sol
@@ -0,0 +1,38 @@
+pragma solidity ^0.4.2;
+
+import "truffle/Assert.sol";
+import "truffle/DeployedAddresses.sol";
+import "../contracts/Voter.sol";
+
+contract TestVoter {
+  function testInitialVotes() public {
+    Voter voter = new Voter(); //Voter(DeployedAddresses.Voter());
+    Assert.equal(voter.getVotes("foo"), 0, "All initial votes should be 0");
+  }
+
+  function testVote() public {
+    Voter voter = new Voter(); //Voter(DeployedAddresses.Voter());
+    voter.vote("foo");
+    Assert.equal(voter.getVotes("foo"), 1, "Voting should increment vote count");
+  }
+
+  function testIndependentVoting() public {
+    Voter voter = new Voter(); // Voter(DeployedAddresses.Voter());
+    voter.vote("foo");
+    voter.vote("bar");
+    Assert.equal(voter.getVotes("foo"), 1, "Voting should be independent among startups (foo)");
+    Assert.equal(voter.getVotes("foo"), 1, "Voting should be independent among startups (bar)");
+  }
+
+  function testEndingVoteResetsVotes() public {
+    Voter voter = new Voter();
+    voter.vote("foo");
+    voter.endVote("11/18/2017");
+    Assert.equal(voter.getVotes("foo"), 0, "Ending the vote resets all votes in voter");
+  }
+
+  // TODO
+  //function testEndingVoteArchivesRecord() public {
+
+  //}
+}
